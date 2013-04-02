6be0e456f868cd995acbcee246cfebc68b6b9e3e
Charles Pick <charles.pick@gmail.com>
2013-04-02 01:06:15 +0100

    include collapse

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("bootstrap-affix");
 require("bootstrap-alert");
 require("bootstrap-button");
 require("bootstrap-carousel");
+require("bootstrap-collapse");
 require("bootstrap-dropdown");
 require("bootstrap-modal");
 require("bootstrap-popover");
